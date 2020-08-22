b4fe7cb91824fd26dc01e797f2e66cc69ad8df3e
Patrick Thomas <pwt5ca@virginia.edu>
2020-08-22 13:01:37 -0400

    Layout fixes

--- src/api/includes/db.php
+++ src/api/includes/db.php
@@ -2,14 +2,14 @@
 
 require "secret.php";
 
-const options = [
+$options = [
   PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
 ];
 
 function GetDatabaseHandler()
 {
   try {
-    $dbh = new PDO(dsn, username, password, options);
+    $dbh = new PDO(dsn, username, password, $options);
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
--- src/index.php
+++ src/index.php
@@ -7,42 +7,55 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>RPi</title>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css">
-  <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+  <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
+  <section class="hero is-primary">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+          rpi.patrickwthomas.net
+        </h1>
+        <h2 class="subtitle">
+          Raspberry Pi Statuses
+        </h2>
+      </div>
+    </div>
+  </section>
+
   <section class="section">
     <div class="container">
 
-      <?php
-      for ($i = 0; $i < sizeof($devices); $i++) {
+      <?php for ($i = 0; $i < sizeof($devices); $i++) {
         $device_name = $devices[$i]['name'];
         $device_global_ip = $devices[$i]['global_ip'];
         $device_local_ip = $devices[$i]['local_ip'];
         $device_date = $devices[$i]['last_check_in'];
-        $status = ((time() - (60*60+1)) < strtotime($device_date)) ? "ALIVE" : "DEAD";
+        $status =
+          time() - (60 * 60 + 1) < strtotime($device_date) ? "ALIVE" : "DEAD";
+        $message_color =
+          time() - (60 * 60 + 1) < strtotime($device_date)
+          ? "is-light"
+          : "is-dark";
+        $tag_color =
+          time() - (60 * 60 + 1) < strtotime($device_date)
+          ? "is-success"
+          : "is-danger";
 
-        echo 
-      "<div class='card'>
-        <div class='card-content'>
-          <div class='media'>
-            <div class='media-content'>
-              <p class='title is-4'>$device_name</p>
-              <p class='subtitle is-6'>$status</p>
-            </div>
+        echo "
+        <article class='message $message_color'>
+          <div class='message-header'>
+            <p>$device_name <span class='tag $tag_color is-medium ml-3'>$status</span></p>
           </div>
-
-          <div class='content'>
-            Global IP: $device_global_ip <br>
-            Local IP: $device_local_ip <br>
-            <time datetime='2020-1-1'>$device_date</time>
+          <div class='message-body'>
+            <p> <span class='icon'> <i class='fa fa-clock-o'></i> </span> $device_date </p>
+            <p> <span class='icon'> <i class='fa fa-globe'></i> </span> $device_global_ip </p>
+            <p> <span class='icon'> <i class='fa fa-sitemap'></i> </span> $device_local_ip </p>
           </div>
-        </div>
-      </div>";
-      }
-      ?>
-
+        </article>
+        ";
+      } ?>
     </div>
   </section>
 </body>
